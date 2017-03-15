21691dabeb9b5a9d37471e0244732fd995796680
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-03-16 07:54:12 +0900

    Test code for code climate

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
-})
+});
+
+var recursion = function() {
+    setTimeout(function log() {
+        console.log('Hello World');
+    }, 100000);
+};
+
+recursion();
 
 app.get('/', function(req, res){
     res.send('Hello World');
