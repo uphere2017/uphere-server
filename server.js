e57be4459f6ffee564a147859c20396febc64189
hyojeong Son <web.sonhyoj@gmail.com>
2017-03-17 14:52:03 +0900

    gulp server update

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
-})
+var server = app.listen(8081, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Express listening on port http://%s:%s', host, port);
+});
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
 module.exports = app; 
