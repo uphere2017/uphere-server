339fb8ec5da32ff4fd7d7f9cf7dc10beb4701a6c
Siwoo <postsw7@gmail.com>
2017-03-19 01:17:40 +0900

    modify appspec.yml

--- deployment/install_dependencies.sh
+++ deployment/install_dependencies.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo chown -R ec2-user: /srv/node/
-cd /srv/node/
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 6.9.4
-sudo npm install
+
+cd ~/node
+npm install
