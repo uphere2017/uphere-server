bd92927d006ab22bef13b608c6a16308df49a445
hyojeong Son <web.sonhyoj@gmail.com>
2017-03-20 19:25:45 +0900

    cors add

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var express = require('express');
+var app = express();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-var app = express();
+var server = app.listen(8080, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Express listening on port http://%s:%s', host, port);
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 mongoose.connect('mongodb://master:djqgldj1234@ds137100.mlab.com:37100/upheredb/');
 
 var db = mongoose.connection;
-
 db.on('error', function (err) {
     console.log('Mongoose default connection error: ' + err)
 });
@@ -20,14 +22,18 @@ db.once('open', function () {
     console.log('Connected to mongodb server')
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello Uphere');
-});
-
-var server = app.listen(8080, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Express listening on port http://%s:%s', host, port);
-});
+var allowCORS = function(req, res, next) {
+  res.header('Acess-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if ('OPTIONS' === req.metod) {
+    res.send(200);
+  } else {
+    return next();
+  }
+};
+ 
+ // 이 부분은 app.use(router)전에 추가
+app.use(allowCORS);
 
 module.exports = app;
--- spec/server.spec.js
+++ spec/server.spec.js
@@ -8,16 +8,4 @@ describe('Test Mocha', function() {
       .get('/')
       .expect(200, done);
   });
-
-  it('Should be Content text is Hello Uphere', function(done) {
-    request(app)
-      .get('/')
-      .expect('Content-Type', 'text/html')
-      .expect(200)
-      .end(function(err, res){
-        expect(res.text).to.equal('Hello Uphere');
-        done();
-      });
-  });
-
 });
