2746dd0dea70cbca934229cb4e0ed14ef28b1687
kmmdong <ahnc87@gmail.com>
2017-03-20 12:29:58 +0900

    test5

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 const app = new express();
 
+app.listen(8080, function() {
+    console.log('Express listening on port http://%s:%s');
+});
 
 app.get('/', function(req, res){
   res.send('Hello World');
-});
-
-app.listen(8080, function() {
-    console.log('Express listening on port http://%s:%s');
 });
 
 module.exports = app;
