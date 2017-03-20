91b09e514ae2ddbc586c63e6b0d12ca70699ed26
kmmdong <ahnc87@gmail.com>
2017-03-20 15:30:04 +0900

    test6

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-aws configure set aws_access_key_id $AWSKEY
-aws configure set aws_secret_access_key $AWSSECRETKEY
-aws configure set default.region ap-northeast-2
-
+if [ $CIRCLE_BRANCH = 'dev' ]; then
+    aws configure set aws_access_key_id $AWSKEY
+    aws configure set aws_secret_access_key $AWSSECRETKEY
+    aws configure set default.region ap-northeast-2
+    aws s3 rm --recursive s3://uphereprod/
+fi
