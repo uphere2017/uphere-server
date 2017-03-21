ea32e8919b59d6c045be996e15c32ff486e25082
JinHo Do <greatdjh@gmail.com>
2017-03-21 19:04:59 +0900

    add GET friend-list api

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ var app = express();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var User = require('./models/user');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var clients = {};
 
-// DB Connection
+mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 mongoose.connect('mongodb://master:djqgldj1234@ds137100.mlab.com:37100/upheredb/');
 
 var db = mongoose.connection;
 
 db.on('error', function (err) {
-  console.log('Mongoose default connection error: ' + err)
+  console.log('Mongoose default connection error: ' + err);
 });
 
 db.once('open', function () {
@@ -25,20 +32,37 @@ var allowCORS = function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  if ('OPTIONS' === req.metod) {
+  if ('OPTIONS' === req.method) {
     res.send(200);
   } else {
     return next();
   }
 };
 
+// 이 부분은 app.use(router)전에 추가
 app.use(allowCORS);
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// WebSocket
-var clients = {};
+/**
+ * GET /users/:user_id/friend-list
+ *
+ * @returns [ { object } ] Each object is friend user data.
+ */
+app.get('/users/:user_id/friend-list', function (req, res) {
+  var userId = req.params.user_id;
+  User.findOne({ _id: userId }).exec()
+    .then(function (userData) {
+      var friends = userData.friend_list.split(',');
+      User.find({ facebook_id: { $in: friends } }).select('-friend_list').exec(function (err, friendList) {
+        if (err) {
+          res.sendStatus(404);
+        }
+        res.json(friendList);
+      });
+    })
+    .catch(function(err) {
+      res.sendStatus(404);
+    });
+});
 
 io.on('connection', function (socket) {
   clients[socket.id] = socket;
