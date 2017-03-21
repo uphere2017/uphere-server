2aba25e6a6185ae1466c4e1ec25df28eda3f5c6a
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-03-21 21:25:35 +0900

    Create a secret route

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,14 @@
 var userController = require('../controllers/userController');
 
 module.exports = function (app) {
-    /**
-     * GET /users/:user_id/friend-list
-     *
-     * @returns [ { object } ] Each object is friend user data.
-     */
-    app.get('/users/:user_id/friend-list', userController.getFriendList);
+  /**
+   * GET /users/:user_id/friend-list
+   *
+   * @returns [ { object } ] Each object is friend user data.
+   */
+  app.get('/users/:user_id/friend-list', userController.getFriendList);
+
+  app.get('/secret', function (req, res) {
+    res.send('Hello, server is live!');
+  });
 };
