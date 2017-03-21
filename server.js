b72cd93f2e8804248b3a717b2244402d7aaa98a5
윤송이 <yunsong-i@yunsong-iui-MacBook-Pro.local>
2017-03-21 16:39:05 +0900

    find query in db and send db in browser
    indent space modify delete console

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Q = require('q');
+var user = require('./models/user.js');
 
 var app = express();
 
@@ -14,14 +16,27 @@ mongoose.connect('mongodb://master:djqgldj1234@ds137100.mlab.com:37100/upheredb/
 var db = mongoose.connection;
 
 db.on('error', function (err) {
-    console.log('Mongoose default connection error: ' + err)
+    console.log('Mongoose default connection error: ' + err);
 });
 db.once('open', function () {
-    console.log('Connected to mongodb server')
+    console.log('Connected to mongodb server');
 });
+
+var findUser = Q.nbind(user.find, user);
+var createUser = Q.nbind(user.create, user);
 
 app.get('/', function (req, res) {
     res.send('Hello Uphere');
+});
+
+app.get('/users/:user_id', function (req, res) {
+  findUser({ name: req.params.user_id })
+    .then(function (data) {
+      res.send(data);
+    })
+    .fail(function (err) {
+      res.send('에러났습니다.');
+    });
 });
 
 var server = app.listen(8080, function () {
