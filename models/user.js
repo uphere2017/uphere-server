b59dbda32e6bba4aa826515cac78a10f1a4dedad
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-03-23 11:51:40 +0900

    Create uphere_id counter mixin

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var counterMixin = require('../database').counterMixin;
 
 var Schema = mongoose.Schema;
 
@@ -12,4 +13,13 @@ var User = new Schema({
   created_at: {type: Date, default: new Date()}
 });
 
-module.exports = mongoose.model('User', User);
+/*
+ * counterMixin automagically creates an unique 'uphere_id' to each model.
+ * Use model instance method 'createWithId' instead of 'save'.
+ * 'createWithId' takes a callback as an argument.
+ *
+ * < example >
+ * var user = new User();
+ * user.createWithId(function (err, user) {});
+*/
+module.exports = counterMixin(mongoose.model('User', User), 'user_id');
