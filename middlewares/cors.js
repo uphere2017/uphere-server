bd741ab64cef9f8ae81fd125c1bdaf18fc8c951c
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-03-24 10:55:05 +0900

    Fix CORS header typo

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 // CORS Headers
 var allowCORS = function (req, res, next) {
-  res.header('Acess-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
