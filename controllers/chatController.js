09439c75ea8f783454f6bb2de7727ac1fe5249c0
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-03-28 11:41:15 +0900

    Controller bug fix and remove userChatController

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,26 +1,92 @@
 var Chat = require('../models/chat');
+var User = require('../models/user');
+var Message = require('../models/message');
 
-var createChatRoom = function (req, res) {
+var createChat = function (req, res) {
   Chat.find({ participants: req.body.participants }, function (err, Chats) {
     if (err) {
       res.sendStatus(404);
     }
-
     if (Chats.length === 0) {
       var chat = new Chat();
       chat.participants = req.body.participants;
-      chat.messages = [];
-      chat.createWithId(function (err, chat) {
-        if (err) {
-          res.sendStatus(500);
-        }
-        res.sendStatus(201).send({ chat_id: chat.uphere_id })
-      });
+      chat.messages = req.body.messages;
+      chat.save()
+        .then(data => {
+          res.status(201).send({ chat_id: data.uphere_id })
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        })
+    } else {
+      res.sendStatus(208);
     }
-    res.sendStatus(208);
   });
 };
 
+var getUserChatList = function (req, res) {
+  var userId = req.params.user_id;
+
+  Chat.find({ participants: { $all: [userId] }})
+    .exec((err, chats) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      var numOfChats = chats.length;
+      var finalResponse = {
+        chats: []
+      };
+
+      if (numOfChats) {
+        for (var i = 0; i < numOfChats; i++) {
+          var chat = chats[i];
+
+          var resChat = {
+            participants: chat.participants,
+            messages: []
+          };
+
+          var numOfMessages = chat.messages.length;
+
+          finalResponse.chats.push(resChat);
+
+          if (numOfMessages) {
+            for (var j = 0; j < numOfMessages; j++) {
+              var message = chat.messages[j];
+
+              Message.find({ uphere_id: message })
+                .exec((err, message) => {
+                  if (err) {
+                    res.status(500).send(err);
+                    j = chat.messages.length;
+                  } else {
+                    resChat.messages.push({
+                      sender_id: message[0].sender_id,
+                      uphere_id: message[0].uphere_id,
+                      text: message[0].text
+                    });
+
+                    if (numOfMessages === resChat.messages.length && finalResponse.chats.length === numOfChats) {
+                      j = chat.messages.length;
+                      i = chats.length;
+                      res.json(finalResponse);
+                    }
+                  }
+                });
+            }
+          } else {
+            res.json(finalResponse);
+            i = chats.length;
+          }
+        }
+      } else {
+        res.json(finalResponse);
+      }
+    });
+};
+
 module.exports = {
-  createChatRoom: createChatRoom
+  getUserChatList: getUserChatList,
+  createChat: createChat
 };
