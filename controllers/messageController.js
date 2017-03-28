9b744869b253c70c22f926bd3555b3b0e09fa577
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-03-28 23:00:36 +0900

    Fix chat bug

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -11,12 +11,12 @@ var postMessageData = function (req, res) {
     if (err) {
       res.sendStatus(500);
     } else {
-      Chat.findOneAndUpdate({ uphere_id: req.params.chat_id }, { $push: { messages: data.uphere_id } })
-        .then(function (msg) {
-          res.status(201).send({ id: data.uphere_id });
+      Chat.findOneAndUpdate({ uphere_id: req.params.chat_id }, { $push: { messages: data.uphere_id } }, { new: true })
+        .then(function (chat) {
+          res.status(201).send({ id: data.uphere_id, text: data.text, created_at: data.created_at, sender_id: data.sender_id });
         })
         .catch(function (err) {
-          res.sendstatus(500);
+          res.sendStatus(500);
         });
     }
   });
