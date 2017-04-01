8b411b66a9ad9b93064308adcea9f5ceeffacd70
Ken Huh <kenhuh@Kens-MacBook-Pro.local>
2017-04-01 17:30:39 +0900

    Add logging

--- sockets/index.js
+++ sockets/index.js
@@ -64,14 +64,30 @@ module.exports = function (server) {
         });
       }
 
+      console.log('[API-LOG] New Message: ', {
+        sender_id: data.sender_id,
+        text: data.text
+      });
+
+      console.log('[API-LOG] Friend UPHERE_ID List:', data.friend_list);
+
       data.friend_list.forEach(function (friendID, i) {
+        console.log('[API-LOG] Friend UPHERE_ID:', friendID);
         var friendSocket = findSocketByUphereID(friendID);
 
         if (friendSocket) {
+          console.log('[API-LOG] Found Friend\'s Socket');
+          console.log('[API-LOG] Emitting FRIEND_EMOTION_CHANGE with data:', {
+            emotion_status: data.emotion_status,
+            friend_id: data.sender_id
+          });
+
           friendSocket.emit(EVENTS.FRIEND_EMOTION_CHANGE, {
             emotion_status: data.emotion_status,
             friend_id: data.sender_id
           });
+        } else {
+          console.log('[API-LOG] Found Friend\'s Socket:', friendID);
         }
       });
     });
