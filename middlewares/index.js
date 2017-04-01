b0f36a3f96b2bba6a39e2a5e34722b810999af56
윤송이 <yunsong-i@yunsong-iui-MacBook-Pro.local>
2017-04-01 15:21:04 +0900

    updownload aws server code

--- middlewares/index.js
+++ middlewares/index.js
@@ -5,5 +5,5 @@ module.exports = function (app) {
   app.use(require('./cors'));
   app.use(morgan('dev'));
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.urlencoded({ parameterLimit: 10000, limit: 1024 * 1024 * 10, extended: true }));
 };
