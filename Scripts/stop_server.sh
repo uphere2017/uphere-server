a5b2801c1171d422c83a9626607d42f8d0143b93
JinHo Do <greatdjh@gmail.com>
2017-04-10 03:07:33 +0900

    change shell command

--- Scripts/stop_server.sh
+++ Scripts/stop_server.sh
@@ -2,7 +2,7 @@
 if forever list | grep /var/nodejs/server/server.js > /dev/null
 then
     echo "Server is running and it will be stopped"
-    forever stop /var/nodejs/server/server.js
+    forever stopall
 else
     echo "Server has been stopped"
 fi
